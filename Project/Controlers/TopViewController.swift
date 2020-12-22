69ad888d5cb117f3a5ffd5440d66b3c27e1f3f7c
Alex <chenchunyi@gmail.com>
2020-12-22 16:43:20 +0800

    Add TopViewController.swift.

---
+++ Project/Controlers/TopViewController.swift
@@ -0,0 +1,38 @@
+//
+//  TopViewController.swift
+//  Project
+//
+//  Created by 陳駿逸 on 2020/12/22.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func typeButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func subTypeButtonPressed(_ sender: Any) {
+    }
+
+    @IBAction func searchButtonPressed(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
